27c91372eecde6e24636dec44e97a7c2ccd3a4b8
tre2man <kimtree97@naver.com>
2019-04-13 12:28:58 +0900

    change delay fun
    1

--- index.js
+++ index.js
@@ -45,18 +45,16 @@ gpio.write(11, true, function(err) {
 	console.log(path.join(__dirname, 'public'));
 	return res.render('index', {status: "온수 버튼 동작"});
     }
-  setTimeout(function(){},500)
-
-  );
-    {
+  setTimeout(function(){
     gpio.write(11, false, function(err) {
             if (err) throw err;
             console.log('Written False to water');
     	console.log(path.join(__dirname, 'public'));
     	return res.render('index',{status: "온수 버튼 동작"});
         });
+  },500)
+);
 
-    }
 });
 
 //app.post('/water/off', function(req, res){
