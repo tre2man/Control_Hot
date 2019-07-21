0c857e197e0de0a353f00ca6b1109e0724bdf966
tre2man <kimtree97@naver.com>
2019-07-22 00:32:53 +0900

    1

--- index.js
+++ index.js
@@ -44,15 +44,6 @@ gpio.write(11, true, function(err) {
         console.log('Written True to water');
 	console.log(path.join(__dirname, 'public'));
 	return res.render('index', {status: "온수 button 클릭"});
-
-//  setTimeout(function() {},500);
-//    gpio.write(11, false, function(err) {
-//          if (err) throw err;
-//          console.log('Written False to water');
-//  	console.log(path.join(__dirname, 'public'));
-//  	return res.render('index',{status: "온수 button 클릭"});
-//      });
-
     });
 
 setTimeout(function() {
